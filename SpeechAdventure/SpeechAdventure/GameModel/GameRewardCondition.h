62938efccdc12ebb16ee4b55b6f2c73a8bed934c
John Chambers <jmchambers90@gmail.com>
2012-08-17 03:55:34 -0700

    Implemented GameRewardCondition. For now, it only compares the actor states required with the current ones, but it has a reference to its parent GameStage so that more checks on more things can be performed.

--- SpeechAdventure/SpeechAdventure/GameModel/GameRewardCondition.h
+++ SpeechAdventure/SpeechAdventure/GameModel/GameRewardCondition.h
@@ -8,6 +8,15 @@
 
 #import <Foundation/Foundation.h>
 
+#import "GameStage.h"
+
 @interface GameRewardCondition : NSObject
+
+@property (nonatomic,strong) NSMutableDictionary *actorStateDictionary;
+@property (nonatomic,strong) GameStage *parentStage;
+
+- (id) initWithStage:(GameStage*)parentStage;
+- (void) addRequiredState:(NSString*)state forActorName:(NSString*)actorName;
+- (BOOL) isConditionSatisfiedWithStage:(GameStage*)myGameStage;
 
 @end
--- SpeechAdventure/SpeechAdventure/GameModel/GameRewardCondition.m
+++ SpeechAdventure/SpeechAdventure/GameModel/GameRewardCondition.m
@@ -10,4 +10,39 @@
 
 @implementation GameRewardCondition
 
+@synthesize actorStateDictionary = _actorStateDictionary;
+@synthesize parentStage = _parentStage;
+
+- (id) init {
+    if (self=[super init]) {
+        self.actorStateDictionary = [[NSMutableDictionary alloc] init];
+    }
+    return self;
+}
+
+- (id) initWithStage:(GameStage*) parentStage {
+    if (self=[self init]) {
+        self.parentStage = parentStage;
+    }
+    return self;
+}
+
+- (void) addRequiredState:(NSString*)state forActorName:(NSString*)actorName {
+    [self.actorStateDictionary setObject:state forKey:actorName];
+}
+
+- (BOOL) isConditionSatisfiedWithStage:(GameStage*)myGameStage {
+    //For now, this only checks whether the required actor states match the current ones
+    for (NSString *actorName in self.actorStateDictionary) {
+        NSString *currentActorState = [myGameStage getActorByName:actorName].state;
+        NSString *requiredState = [self.actorStateDictionary objectForKey:actorName];
+        
+        if (![requiredState isEqualToString:currentActorState]) {
+            return NO;
+        }
+    }
+    
+    return YES;
+}
+
 @end
